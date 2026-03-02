ecc603eecc38659542ff56afa75264c4a0f4f2e9
hburn7 <38370573+hburn7@users.noreply.github.com>
2026-03-01 22:47:37 -0500

    Support CSV uploads and new omc-api changes

--- src/api.py
+++ src/api.py
@@ -1,7 +1,9 @@
+import os
 from dataclasses import dataclass
 from typing import Optional
-import os
+
 import aiohttp
+
 
 @dataclass
 class ValidationResponse:
@@ -15,39 +17,83 @@ class ValidationResponse:
     cover: Optional[str] = None
     artist: Optional[str] = None
     title: Optional[str] = None
+    artist_unicode: Optional[str] = None
+    title_unicode: Optional[str] = None
     ownerId: Optional[int] = None
     ownerUsername: Optional[str] = None
     status: str = ""
+
+
+@dataclass
+class RawValidationResponse:
+    complianceStatus: str
+    complianceStatusString: str
+    artist: str
+    title: str
+    artist_unicode: str
+    title_unicode: str
+    complianceFailureReason: Optional[str] = None
+    complianceFailureReasonString: Optional[str] = None
+    notes: Optional[str] = None
+
 
 @dataclass
 class ApiResponse:
     results: list[ValidationResponse]
     failures: list[int]
 
+
 # API response:
 # - Types: https://github.com/hburn7/omc-api/blob/6ca27c3ac58f0ece8616eacf37ff4c1a7e7b7a32/src/lib/dataTypes.ts#L33
 # - Response: https://github.com/hburn7/omc-api/blob/master/index.ts#L57
-async def validate(beatmap_ids: list[int]) -> Optional[ApiResponse]:
+async def validate(
+    beatmap_ids: list[int], strict: bool = False
+) -> Optional[ApiResponse]:
     secret = os.getenv("API_SECRET")
     if not secret:
         return None
-    
+
     endpoint = f"{os.getenv('API_URL')}/validate"
+    if strict:
+        endpoint += "?strict=true"
 
     async with aiohttp.ClientSession() as session:
-        async with session.post(endpoint, json=beatmap_ids, headers={
-            'X-Api-Key': secret
-        }) as response:
+        async with session.post(
+            endpoint, json=beatmap_ids, headers={"X-Api-Key": secret}
+        ) as response:
             if response.status != 200:
                 data = await response.json()
-                print(f'Failed to validate beatmaps due to non-200 status code: {data}')
+                print(f"Failed to validate beatmaps due to non-200 status code: {data}")
                 return None
 
             data = await response.json()
-            all_results = [ValidationResponse(**result) for result in data.get('results', [])]
-            all_failures = data.get('failures', [])
+            all_results = [
+                ValidationResponse(**result) for result in data.get("results", [])
+            ]
+            all_failures = data.get("failures", [])
 
-            return ApiResponse(
-                results=all_results,
-                failures=all_failures
-            )
+            return ApiResponse(results=all_results, failures=all_failures)
+
+
+async def validate_metadata(
+    inputs: list[dict], strict: bool = False
+) -> Optional[list[RawValidationResponse]]:
+    secret = os.getenv("API_SECRET")
+    if not secret:
+        return None
+
+    endpoint = f"{os.getenv('API_URL')}/validate-metadata"
+    if strict:
+        endpoint += "?strict=true"
+
+    async with aiohttp.ClientSession() as session:
+        async with session.post(
+            endpoint, json=inputs, headers={"X-Api-Key": secret}
+        ) as response:
+            if response.status != 200:
+                data = await response.json()
+                print(f"Failed to validate metadata due to non-200 status code: {data}")
+                return None
+
+            data = await response.json()
+            return [RawValidationResponse(**item) for item in data]
